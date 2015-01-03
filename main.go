0447d94254ba5016e4a48e83a129b6a85c698e34
Sam Blazes <sunblazes@gmail.com>
2015-01-02 22:16:41 -0500

    Added comments, cleaned up some code, and fixed a bug.

--- main.go
+++ main.go
@@ -10,9 +10,11 @@ import (
 )
 
 func main() {
-    thread_count := 0
+
+    //holds all the pages for BFS
     queue := make(chan []string, 100000000)
 
+    //regex to extract exclusively wikipedia links from fetched pages
     reFindLink, err := regexp.Compile("href=\"(/wiki/[^\"/ :#]*)\"")
 
     if err != nil {
@@ -20,29 +22,41 @@ func main() {
         return
     }
 
+    //To prevent undefined behavior of concurrently used maps
     mutex := new(sync.Mutex)
 
+    //list of pages the program has already visited, stored as wikipedia.org suffixes
     visited := make(map[string]bool)
 
+    //link to add suffixes onto
     base_link := "https://en.wikipedia.org"
 
+    //page to start at
     http_start := []string{"/wiki/Raspberry_Pi"}
 
+    //page to find
     link_goal := "/wiki/Egypt"
 
     queue <- http_start
 
     done := false
 
+    //Value to keep track of distance from original page
     level := 1
+
+    thread_count := 512
+
+    running_threads := 0;
 
     var solution []string
 
-    for i := 0; i < 512; i++ {
-        thread_count++
+    for i := 0; i < thread_count; i++ {
+
+    	//Start thread
+    	running_threads++
         fmt.Printf("[%2d] Launching...\n", i)
         go func (id int) {
-            
+            defer func() {running_threads--}()
             fmt.Printf("[%2d] Starting...\n", id)
 
             for L := range queue {
@@ -50,10 +64,15 @@ func main() {
                     level++
                     fmt.Println("Level:", level, "Len:", len(queue))
                 }
+                //Fetch Wikipedia page
                 req, err := http.Get(base_link + L[len(L)-1])
-                if err == nil {
+                if err != nil {
+                	//Extract Links
                     b, _ := ioutil.ReadAll(req.Body)
                     links := reFindLink.FindAllStringSubmatch(string(b), -1)
+
+                    //Add links to queue in batches
+                    //mutex lock to prevent concurrent map usage
                     mutex.Lock()
                     for t := range links {
                         _, v := visited[links[t][1]]
@@ -82,8 +101,10 @@ func main() {
         }(i)
     }
 
+    //wait for threads to find the solution
+    ///TODO: should probably find a better way to do this (maybe mutex?)
     for !done {
-        time.Sleep(10)
+        time.Sleep(1)
     }
 
     fmt.Println(solution)
